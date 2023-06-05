ab76351a6095e888b562ec7c28ff3cd661c162b3
Merlin <merlin.vop@gmail.com>
2023-06-05 21:53:14 +0300

    Транзакції

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,33 @@
 import Profile from 'components/Profiles/Profile';
 import Statistics from 'components/Statistics/Statistics';
 import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/Transaction/TransactionHistory';
 import user from 'data/user';
 import data from 'data/data';
 import friends from 'data/friends';
+import transactions from 'data/transactions';
 import css from 'components/App.module.css';
 
 export const App = () => {
   return (
-    <div className={css.wrapper}>
-      <div>
-        <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-        <Statistics title="Upload stats" stats={data} />
+    <div>
+      <div className={css.wrapper}>
+        <div>
+          <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+          />
+          <Statistics title="Upload stats" stats={data} />
+        </div>
+        <div>
+          <FriendList friends={friends} />
+        </div>
       </div>
       <div>
-        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />;
       </div>
     </div>
   );
